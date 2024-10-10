f45a34ab50566518082d444754f1cd344ebb969a
mohamed elmasry <itsjust4me13@gmail.com>
2024-10-10 13:27:06 +0300

    fix database

--- database/migrations/xxxx_xx_xxx_make_category_id_nullable_in_projects_table.php
+++ database/migrations/xxxx_xx_xxx_make_category_id_nullable_in_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class MakeCategoryIdNullableInProjectsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -37,4 +37,4 @@ class MakeCategoryIdNullableInProjectsTable extends Migration
             $table->integer('category_id')->nullable()->change();
         });
     }
-}
+};
